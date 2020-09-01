e87308b315b758271ae36a337e569316577650b9
Anthony K <antonyk@users.noreply.github.com>
2020-09-01 12:09:25 -0400

    prints if a number is prime

---
+++ src/16_prime.py
@@ -0,0 +1,17 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    if num == 2:
+        return True
+
+    cur = 2
+    while cur < num:
+        if num % cur == 0:
+            return False
+        cur += 1
+
+    return True
+
+
+num = int(input(f"Enter a number to test: "))
+print(is_prime(num))
