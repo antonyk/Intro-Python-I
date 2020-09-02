cf95cd43d471fca46626fb29c3f95c5f635162c8
Anthony K <antonyk@users.noreply.github.com>
2020-09-02 19:00:14 -0400

    refactor with cache

--- src/16_prime.py
+++ src/16_prime.py
@@ -1,4 +1,7 @@
-def is_prime(num):
+import datetime
+
+
+def is_prime_slow(num):
     if num < 2:
         return False
     if num == 2:
@@ -12,14 +15,52 @@ def is_prime(num):
 
     return True
 
+
+def is_prime_fast(num, cache):
+    if num in cache:
+        return cache[num]
+
+    if num < 2:
+        return False
+    if num == 2:
+        return True
+
+    cur = 2
+    while cur < num:
+        if num % cur == 0:
+            cache[num] = False
+            return False
+        cur += 1
+
+    cache[num] = True
+    return True
+
+
 # num = int(input(f"Enter a number to test: "))
 # print(is_prime(num))
 
+def test_primes():
+    num = int(input(f"Enter max number to test: "))
 
-prim = []
-num = int(input(f"Enter max number to test: "))
-for i in range(num):
-    if is_prime(i):
-        prim.append(i)
+    # slow
+    start = datetime.datetime.now()
+    primes = []
+    for i in range(num):
+        if is_prime_slow(i):
+            primes.append(i)
+    end = datetime.datetime.now()
+    print(str(end-start))
 
-print(prim)
+    # slow
+    start = datetime.datetime.now()
+    primes = []
+    cache = {}
+    for i in range(num):
+        if is_prime_fast(i, cache):
+            primes.append(i)
+    end = datetime.datetime.now()
+    print(str(end-start))
+
+
+# print(primes)
+test_primes()
